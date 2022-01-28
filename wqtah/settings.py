a085be139e5aac5a7cb082c473abba56287bab84
shantanukiwitech17 <94366590+shantanukiwitech17@users.noreply.github.com>
2022-01-28 16:55:30 +0530

    Update settings.py
    update CORS

--- wqtah/settings.py
+++ wqtah/settings.py
@@ -234,8 +234,32 @@ CORS_EXPOSE_HEADERS = {
     'Access-Control-Allow-Headers: *'
 }
 
-CORS_ORIGIN_WHITELIST = [
-    'https://wqtah-dev-frontend.kiwi-internal.com'
-]
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
 
+# enable cors
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = ['*']
 
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
+
+CORS_ALLOW_CREDENTIALS = True
+
+# define SECURE PROXY SSL HEADER
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
